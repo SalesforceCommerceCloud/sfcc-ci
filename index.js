e9563db27151ab6f30558d906ab200d5ef0cc329
Daniel Mersiowsky <dmersiowsky@salesforce.com>
2017-10-04 14:50:01 +0200

    Fixes for Script API:  - fixed index.js typos  - changed APIs to be conform to each other (callback params, error behaviour/return values)  - fixed "code version already active" behaviour (now similar to "success")  - fixed "instance.import" logic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // make the APIs publicly available
-exports.auth = require('lib/auth').api;
-exports.instance = require('lib/job').instance;
-exports.code = require('lib/code').api;
-exports.job = require('lib/job').api;
+exports.auth = require('./lib/auth').api;
+exports.instance = require('./lib/instance').api;
+exports.code = require('./lib/code').api;
+exports.job = require('./lib/job').api;

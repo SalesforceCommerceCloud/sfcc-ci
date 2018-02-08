98afc14c73584e0589172d250f77f476d378444e
Tobias Lohr <tlohr@demandware.com>
2018-02-08 22:14:48 +0100

    more cli functional tests, option to limit/expand scope of tests to allow quick change based tests and nightly regression

--- bin/test-cli.sh
+++ bin/test-cli.sh
@@ -1,12 +1,38 @@
 #!/usr/bin/env bash
 
 ###############################################################################
+###### Bootstrap
+###############################################################################
+
+# ensure jq is available
+TEST_JQ=`hash jq`
+if [ $? -eq 1 ]; then
+	echo -e "jq is required to run the cli tests. Get jq from https://stedolan.github.io/jq/"
+	exit 1
+fi
+
+# mapping input parameters
+TEST_SCOPE=$1
+TEST_CLIENT_ID=$2
+TEST_CLIENT_SECRET=$3
+TEST_HOST=$4
+
+# scope of tests, either 'minimal' or 'full'
+if [ "$TEST_SCOPE" = "minimal" ]; then
+	echo -e "Running default test scope with limited coverage of commands and options..."
+elif [ "$TEST_SCOPE" = "full" ]; then
+	echo -e "Running full test scope with maximum coverage of commands and options..."
+else
+	echo -e "Unknown test scope $TEST_SCOPE. Please provide either 'minimal' or 'full'."
+	exit 1
+fi
+
+###############################################################################
 ###### Testing ´sfcc-ci client:auth´
 ###############################################################################
 
-# authorize client using client:auth, uses params $1 (client_id), $2 (client_secret)
-echo "Testing command ´sfcc-ci client:auth´:"
-node ./cli.js client:auth $1 $2
+echo "Testing command ´sfcc-ci client:auth´ without option:"
+node ./cli.js client:auth $TEST_CLIENT_ID $TEST_CLIENT_SECRET
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -19,8 +45,8 @@ fi
 ###############################################################################
 
 echo "Testing command ´sfcc-ci client:auth:token´:"
-TEST_TOKEN=`node ./cli.js client:auth:token`
-if [ $? -eq 0 ] && [ ! -z "$TEST_TOKEN" ]; then
+TEST_RESULT=`node ./cli.js client:auth:token`
+if [ $? -eq 0 ] && [ ! -z "$TEST_RESULT" ]; then
     echo -e "\t> OK"
 else
 	echo -e "\t> FAILED"
@@ -31,7 +57,6 @@ fi
 ###### Testing ´sfcc-ci client:renew´
 ###############################################################################
 
-# do an auth renewal using client:auth:renew, uses pre-authorized client auth, no parameters
 echo "Testing command ´sfcc-ci client:renew´ (expected to fail):"
 node ./cli.js client:auth:renew
 if [ $? -eq 1 ]; then
@@ -41,9 +66,8 @@ else
 	exit 1
 fi
 
-# authorize client using client:auth with --renew flag, uses params $1 (client_id), $2 (client_secret)
-echo "Testing command ´sfcc-ci client:auth´ with flag --renew:"
-node ./cli.js client:auth $1 $2 --renew
+echo "Testing command ´sfcc-ci client:auth´ with --renew option:"
+node ./cli.js client:auth $TEST_CLIENT_ID $TEST_CLIENT_SECRET --renew
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -51,7 +75,6 @@ else
 	exit 1
 fi
 
-# do an auth renewal using client:auth:renew, uses pre-authorized client auth, no parameters
 echo "Testing command ´sfcc-ci client:renew´ (expected to succeed):"
 node ./cli.js client:auth:renew
 if [ $? -eq 0 ]; then
@@ -65,7 +88,6 @@ fi
 ###### Testing ´sfcc-ci auth:logout´
 ###############################################################################
 
-# clears any client settings, using auth:logout, uses pre-authorized client auth, no parameters
 echo "Testing command ´sfcc-ci auth:logout´:"
 node ./cli.js auth:logout
 if [ $? -eq 0 ]; then
@@ -79,9 +101,8 @@ fi
 ###### Testing ´sfcc-ci instance:clear´
 ###############################################################################
 
-# add an instance, using instance:add, uses param $3 (instance)
 echo "Testing command ´sfcc-ci instance:add´ (without alias):"
-node ./cli.js instance:add $3
+node ./cli.js instance:add $TEST_HOST
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -89,7 +110,6 @@ else
 	exit 1
 fi
 
-# clear instances, using instance:clear
 echo "Testing command ´sfcc-ci instance:clear´:"
 node ./cli.js instance:clear
 if [ $? -eq 0 ]; then
@@ -103,9 +123,8 @@ fi
 ###### Testing ´sfcc-ci instance:add´
 ###############################################################################
 
-# add an instance, using instance:add with alias option, uses param $3 (instance) and hardcoded alias "my"
 echo "Testing command ´sfcc-ci instance:add´ (with alias):"
-node ./cli.js instance:add $3 my
+node ./cli.js instance:add $TEST_HOST my
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -113,7 +132,6 @@ else
 	exit 1
 fi
 
-# add an invalid instance, using instance:add, uses hardcoded instance
 echo "Testing command ´sfcc-ci instance:add´ with invalid instance (expected to fail):"
 node ./cli.js instance:add my-instance.demandware.net
 if [ $? -eq 1 ]; then
@@ -123,9 +141,8 @@ else
 	exit 1
 fi
 
-# add another valid instance, using instance:add, uses param $3 (instance)
 echo "Testing command ´sfcc-ci instance:add´:"
-node ./cli.js instance:add $3 someotheralias
+node ./cli.js instance:add $TEST_HOST someotheralias
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -137,9 +154,8 @@ fi
 ###### Testing ´sfcc-ci instance:set´
 ###############################################################################
 
-# set instance, using instance:set, uses param $3 (instance)
-echo "Testing command ´sfcc-ci instance:set´:"
-node ./cli.js instance:set $3
+echo "Testing command ´sfcc-ci instance:set´ with host name:"
+node ./cli.js instance:set $TEST_HOST
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -147,8 +163,7 @@ else
 	exit 1
 fi
 
-# set instance, using instance:set, uses hardcoded alias "my"
-echo "Testing command ´sfcc-ci instance:set´:"
+echo "Testing command ´sfcc-ci instance:set´ with alias:"
 node ./cli.js instance:set my
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
@@ -162,10 +177,9 @@ fi
 ###############################################################################
 
 # the next set of tests are testing real interactions with a Commerce Cloud instance
-# re-authorize first using client:auth, uses params $1 (client_id), $2 (client_secret)
-# this ensure, that we have a proper authentication
+# re-authorize first using client:auth, this ensure, that we have a proper authentication
 echo "Testing command ´sfcc-ci client:auth´ again:"
-node ./cli.js client:auth $1 $2
+node ./cli.js client:auth $TEST_CLIENT_ID $TEST_CLIENT_SECRET
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
 else
@@ -173,7 +187,6 @@ else
 	exit 1
 fi
 
-# site import upload, using instance:upload, uses hardcoded test file
 echo "Testing command ´sfcc-ci instance:upload´:"
 node ./cli.js instance:upload ./test/cli/site_import.zip
 if [ $? -eq 0 ]; then
@@ -183,8 +196,16 @@ else
 	exit 1
 fi
 
-# site import upload, using instance:upload, and non existing file
-echo "Testing command ´sfcc-ci instance:upload´ (expected to fail):"
+echo "Testing command ´sfcc-ci instance:upload´ with --instance option:"
+node ./cli.js instance:upload ./test/cli/site_import.zip --instance $TEST_HOST
+if [ $? -eq 0 ]; then
+    echo -e "\t> OK"
+else
+	echo -e "\t> FAILED"
+	exit 1
+fi
+
+echo "Testing command ´sfcc-ci instance:upload´ with non-existing file (expected to fail):"
 node ./cli.js instance:upload ./test/does/not/exist/site_import.zip
 if [ $? -eq 1 ]; then
     echo -e "\t> OK"
@@ -193,19 +214,25 @@ else
 	exit 1
 fi
 
-# TODO site import upload with -i flag
-
 ###############################################################################
 ###### Testing ´sfcc-ci instance:state:save´
 ###############################################################################
 
-# TODO
+if [ $TEST_SCOPE = "full" ]; then
+	echo "Testing command ´sfcc-ci instance:state:save´ with --sync option:"
+	node ./cli.js instance:state:save --sync
+	if [ $? -eq 0 ]; then
+		echo -e "\t> OK"
+	else
+		echo -e "\t> FAILED"
+		exit 1
+	fi
+fi
 
 ###############################################################################
 ###### Testing ´sfcc-ci instance:import´
 ###############################################################################
 
-# site import, using instance:import, uses hardcoded test file
 echo "Testing command ´sfcc-ci instance:import´ without options:"
 node ./cli.js instance:import site_import.zip
 if [ $? -eq 0 ]; then
@@ -215,7 +242,14 @@ else
 	exit 1
 fi
 
-# TODO instance:import with -i flag
+echo "Testing command ´sfcc-ci instance:import´ with --instance option:"
+node ./cli.js instance:import site_import.zip --instance $TEST_HOST
+if [ $? -eq 0 ]; then
+    echo -e "\t> OK"
+else
+	echo -e "\t> FAILED"
+	exit 1
+fi
 
 echo "Testing command ´sfcc-ci instance:import´ with --sync option:"
 node ./cli.js instance:import site_import.zip --sync
@@ -235,20 +269,36 @@ else
 	exit 1
 fi
 
-# TODO instance:import with -s and -j flags
+echo "Testing command ´sfcc-ci instance:import´ with --json and --sync option:"
+TEST_RESULT=`node ./cli.js instance:import site_import.zip --json --sync | jq '.exit_status.code' -r`
+if [ $? -eq 0 ] && [ "$TEST_RESULT" = "OK" ]; then
+    echo -e "\t> OK"
+else
+	echo -e "\t> FAILED"
+	echo -e "\t> Test result was: $TEST_RESULT"
+	exit 1
+fi
 
 ###############################################################################
 ###### Testing ´sfcc-ci instance:state:reset´
 ###############################################################################
 
-# TODO
+if [ $TEST_SCOPE = "full" ]; then
+	echo "Testing command ´sfcc-ci instance:state:reset´ with --sync option:"
+	node ./cli.js instance:state:reset --sync
+	if [ $? -eq 0 ]; then
+		echo -e "\t> OK"
+	else
+		echo -e "\t> FAILED"
+		exit 1
+	fi
+fi
 
 ###############################################################################
 ###### Testing ´sfcc-ci code:deploy´
 ###############################################################################
 
-# code deploy, using code:deploy, uses hardcoded test file
-echo "Testing command ´sfcc-ci code:deploy´:"
+echo "Testing command ´sfcc-ci code:deploy´ without option:"
 node ./cli.js code:deploy ./test/cli/custom_code.zip
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
@@ -257,8 +307,7 @@ else
 	exit 1
 fi
 
-# code deploy, using code:deploy, and non existing file
-echo "Testing command ´sfcc-ci code:deploy´ (expected to fail):"
+echo "Testing command ´sfcc-ci code:deploy´ with non-existing file (expected to fail):"
 node ./cli.js code:deploy ./test/does/not/exist/custom_code.zip
 if [ $? -eq 1 ]; then
     echo -e "\t> OK"
@@ -267,20 +316,34 @@ else
 	exit 1
 fi
 
-# TODO code deploy with -i flag
+echo "Testing command ´sfcc-ci code:deploy´ with --instance option:"
+node ./cli.js code:deploy ./test/cli/custom_code.zip --instance $TEST_HOST
+if [ $? -eq 0 ]; then
+    echo -e "\t> OK"
+else
+	echo -e "\t> FAILED"
+	exit 1
+fi
 
 ###############################################################################
 ###### Testing ´sfcc-ci code:list´
 ###############################################################################
 
-# TODO
+echo "Testing command ´sfcc-ci code:list´ with --json option:"
+TEST_RESULT=`node ./cli.js code:list --json | jq '.count'`
+if [ $? -eq 0 ] && [ $TEST_RESULT > 0 ]; then
+    echo -e "\t> OK"
+else
+	echo -e "\t> FAILED"
+	echo -e "\t> Test result was: $TEST_RESULT"
+	exit 1
+fi
 
 ###############################################################################
 ###### Testing ´sfcc-ci code:activate´
 ###############################################################################
 
-# code activate, using code:activate and hard coded code version
-echo "Testing command ´sfcc-ci code:activate´:"
+echo "Testing command ´sfcc-ci code:activate´ without option:"
 node ./cli.js code:activate modules
 if [ $? -eq 0 ]; then
     echo -e "\t> OK"
@@ -289,9 +352,15 @@ else
 	exit 1
 fi
 
-# TODO code activate with -i flag
+echo "Testing command ´sfcc-ci code:activate´ with --instance option:"
+node ./cli.js code:activate modules --instance $TEST_HOST
+if [ $? -eq 0 ]; then
+    echo -e "\t> OK"
+else
+	echo -e "\t> FAILED"
+	exit 1
+fi
 
-# code activate, using code:activate and non existing code version
 echo "Testing command ´sfcc-ci code:activate´´ with invalid version (expected to fail):"
 node ./cli.js code:activate does_not_exist
 if [ $? -eq 1 ]; then
